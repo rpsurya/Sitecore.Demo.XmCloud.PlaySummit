0cb058fe5321d6e34030b31e430637d2b4c7be29
Surya <sgahlot@rightpoint.com>
2023-10-19 18:49:45 -0400

    fixed prettier error

--- src/rendering/src/components/MyComponent/MyComponent.tsx
+++ src/rendering/src/components/MyComponent/MyComponent.tsx
@@ -1,10 +1,10 @@
 import { Text, Field, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
 type MyComponentProps = {
   fields: {
-    heading: Field<string>,
-    body: Field<string>  
+    heading: Field<string>;
+    body: Field<string>;
   };
-}
+};
 const MyComponent = (props: MyComponentProps): JSX.Element => (
   <div>
     <Text field={props.fields.heading} />
